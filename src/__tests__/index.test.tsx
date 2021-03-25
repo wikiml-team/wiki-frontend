7a56764d4703948c6a212c79bfc6c8b853f80f73
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-03-24 21:35:21 -0500

    chore: setting enzyme right config.

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,4 +1,16 @@
 import React from "react";
 import App from "../App";
 
-describe("Index", () => {});
+import { ReactWrapper, shallow, ShallowWrapper } from "enzyme";
+
+describe("Index", () => {
+  let wrapperApp: ReactWrapper | ShallowWrapper;
+
+  beforeEach(() => {
+    wrapperApp = shallow(<App />);
+  });
+
+  it("renders the App", () => {
+    expect(wrapperApp).toHaveLength(1);
+  });
+});
