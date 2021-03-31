0ab4bab7bf9209c603b6bc310b980e08fc6a58ac
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-03-31 18:51:58 -0500

    feat-test IState: adding state interface.

---
+++ src/models/__tests__/state.test.ts
@@ -0,0 +1,7 @@
+import State from "../state";
+
+describe("State Interface", () => {
+  it("passes", () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/models/state.ts
@@ -0,0 +1,7 @@
+export default interface IState {
+  // lang: string;
+  theme: { value: "default" | "red" | "cyan" | "blue" | "orange" | "teal" };
+  // authenticated: boolean;
+  // notifications: Notification[];
+  // navigation: JSX.Element;
+}
