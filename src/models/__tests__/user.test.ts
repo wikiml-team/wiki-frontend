d4919ae784d3034288035cc7dcf18f697be3b81e
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-03-31 23:09:03 -0500

    feat IUser: adding basic user model.

---
+++ src/models/__tests__/user.test.ts
@@ -0,0 +1,7 @@
+import IUser from "../user";
+
+describe("User Interface", () => {
+  it("passes", () => {
+    expect(1).toBe(1);
+  });
+});
---
+++ src/models/user.ts
@@ -0,0 +1,7 @@
+export default interface IUser {
+  name: string;
+  nickname: string;
+  picture: string;
+  coincolor: string;
+  email: string;
+}
