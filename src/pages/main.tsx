81cf67a57cf238c9ee3cb46adc7f51b4384467a0
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-05-04 00:09:45 -0500

    fix workplace-link.

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Main() {
   return (
     <React.Fragment>
-      <a href="/workplace">Workplace</a>
+      <Link to="/workplace">Workplace</Link>
     </React.Fragment>
   );
 }
