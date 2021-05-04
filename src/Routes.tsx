c4f758b222a96a14ad5f0ba0158516339287ca85
Gabriela Rodríguez Santa <gabisantacruzpacheco@gmail.com>
2021-05-04 00:03:00 -0500

    chore gh-pages.

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,6 +1,6 @@
 import main from "pages/main";
 
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
 import WorkplaceLayout from "layouts/workplace";
 
