459d36b21f03bf0d692ebd23b19e74cb61c97507
Gabriela Rodriguez <gabi.santacruzpacheco@gmail.com>
2021-09-22 03:27:07 +0200

    feat NewPage.

--- src/pages/home/new.tsx
+++ src/pages/home/new.tsx
@@ -1,23 +1,34 @@
-import { useQuery } from '@apollo/client';
-import { GET_METHODOLOGIES } from 'apollo/methodologies';
 import React from 'react'
+import { useQuery } from '@apollo/client';
 
-// New: list methodologies
+import { GET_METHODOLOGIES } from 'apollo/methodologies';
+import { Stack, Text } from '@fluentui/react';
+import MethodologyPage from 'components/cards/methodologycard';
+import { useTranslation } from 'react-i18next';
 
 export default function NewPage() {
+        
+    const { t } = useTranslation("navbar");
+
+    return <React.Fragment>
+        <Text variant='xLarge'>{t("new")}</Text>
+        <Stack horizontal>
+            <NewProjects />
+        </Stack>
+    </React.Fragment>
+}
+
+const NewProjects = () => {
 
     const {loading, error, data} = useQuery(GET_METHODOLOGIES);
   
     if (loading) return <p>Loading...</p>;
     if (error) return <p>Error :(</p>;
 
-    return <>New</>
-    // return data.methodologies.map(({ id , name } : any) =>  (
-    //     <div key={id}>
-    //       <p>
-    //         {id}: {name}
-    //       </p>
-    //     </div>
-    //     )
-    // )
+    return data.methodologies.map(({ id , name } : any) =>  (
+        <Stack.Item key={id} >
+          <MethodologyPage name={name} />
+        </Stack.Item>
+        )
+    )
 }
