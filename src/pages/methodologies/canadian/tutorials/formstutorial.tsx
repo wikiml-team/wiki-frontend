dbe9b75083482b942a26b7b9e63f0fa65d5a485b
Gabriela Rodriguez <gabi.santacruzpacheco@gmail.com>
2021-09-24 12:47:30 +0200

    feat FormsTutorial.

--- src/pages/methodologies/canadian/tutorials/formstutorial.tsx
+++ src/pages/methodologies/canadian/tutorials/formstutorial.tsx
@@ -1,5 +1,45 @@
-import React from "react";
+import { useTranslation } from "react-i18next";
+import { DetailsList, IColumn, SelectionMode, Text } from "@fluentui/react";
+import { useSelector } from "react-redux";
+import { selectWorkplaceConfig } from "store/slices/workplaceslice";
+
+interface IForm {
+  key: string;
+  name: string
+}
 
 export default function FormsTutorials() {
-  return <div>Forms Tutorial</div>;
+  // LOGIC
+  const { t } = useTranslation("pages");
+
+  const { tabsSchema } = useSelector(selectWorkplaceConfig);
+
+  const forms : IForm[] = tabsSchema.findByKey("key4").childtabs?.map((c, i) => {
+    return ({ key: `key${i}`, name: c.name} as IForm)}) || [] as IForm[]
+
+  const columns: IColumn[] = [
+    {
+        key: 'column1',
+        name: 'forms',
+        fieldName: 'forms',
+        minWidth: 100,
+        data: 'string',
+        onRender: (item: IForm) => formRender(item)
+    },
+  ]
+
+  const formRender = (form: IForm) => {
+    return (
+      <Text variant="medium">
+       {t(form.name)}
+    </Text>
+    )
+  }
+
+  return <DetailsList
+            items={forms}
+            columns={columns}
+            selectionMode={SelectionMode.none}
+            isHeaderVisible={false}
+        />
 }
