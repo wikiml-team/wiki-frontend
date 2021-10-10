60bb1d8ac2c9dd2e74b58062978ea2cc00798cd9
Gabriela Rodriguez <gabi.santacruzpacheco@gmail.com>
2021-10-10 20:39:37 +0200

    feat MethodologyCardHover.

--- src/components/cards/methodologycard.tsx
+++ src/components/cards/methodologycard.tsx
@@ -4,13 +4,21 @@ import { DocumentCard,
     DocumentCardImage, 
     DocumentCardTitle, 
     IDocumentCardStyles, 
-    ImageFit
+    ImageFit,
+    PrimaryButton,
+    mergeStyleSets,
+    DefaultButton,
+    Stack,
+    IButtonStyles,
+    useTheme
 } from '@fluentui/react'
+
 import canadian from "./logos/canadian.png"
 import german from "./logos/german.png"
 import autralian from "./logos/australian.png"
-import React from 'react'
-
+import styled from 'styled-components'
+import { useState } from 'react'
+import { Centered, CenteredHorizontal } from 'components/styled/centered'
 
 type MethodologyCardProps = {
     name : string,
@@ -27,31 +35,79 @@ const pic_dict = {
 export default function MethodologyCard(props : MethodologyCardProps) {
 
   const { name, href, onClick } = props;
+  const { palette } = useTheme()
 
-    const cardStyles: IDocumentCardStyles = {
-        root: { 
-            display: 'inline-block', 
-            margin: "20px 20px 20px 0", 
-            width: 250 
-        },
-      };
+  const [displayContent, setDisplayContent] = useState(false)
+  
+  const classes = mergeStyleSets({
+      cardContent: {
+        opacity: displayContent? "35%" : "inherit"
+      }
+  })
+
+  const cardStyles: IDocumentCardStyles = {
+      root: { 
+          display: 'inline-block', 
+          margin: "20px 20px 20px 0", 
+          width: 250 
+      },
+  };
+
+  const buttonStyles : IButtonStyles = {
+    root: {
+      backgroundColor: palette.themeDark
+    }
+  }
+
 
   return (
-    <React.Fragment>
       <DocumentCard
           onClickHref={href}
           onClick={onClick}
           styles={cardStyles}
+          onMouseEnter={() => setDisplayContent(true)}
+          onMouseLeave={() => setDisplayContent(false)}
         >
-          <DocumentCardImage 
-            height={90} 
-            imageFit={ImageFit.cover} 
-            imageSrc={pic_dict["canadian"]} 
-          />
-          <DocumentCardDetails>
-            <DocumentCardTitle title={name} showAsSecondaryTitle />
-          </DocumentCardDetails>
+          <div className={classes.cardContent}>
+            <DocumentCardImage 
+              height={90} 
+              imageFit={ImageFit.cover} 
+              imageSrc={pic_dict["canadian"]} 
+              />
+            <DocumentCardDetails>
+              <DocumentCardTitle title={name} showAsSecondaryTitle />
+            </DocumentCardDetails>
+          </div>
+
+          {displayContent && (<DisplayOver>
+            <Centered>
+
+              <Stack horizontal tokens={{childrenGap: 10}}>
+                <PrimaryButton
+                  text="Read"
+                  onClick={()=> {}} 
+                  styles={buttonStyles}
+                  />
+                <PrimaryButton 
+                  text="Edit" 
+                  onClick={()=> {}} 
+                  />
+              </Stack>
+            </Centered>
+          </DisplayOver>)}
       </DocumentCard>
-    </React.Fragment>
   )
 }
+
+const DisplayOver = styled.div({
+  height: "100%",
+  left: "0",
+  position: "absolute",
+  top: "0",
+  width: "100%",
+  zIndex: 2,
+  transition: "background-color 350ms ease",
+  backgroundColor: "transparent",
+  padding: "20px 20px",
+  boxSizing: "border-box",
+});
--- src/components/styled/centered.tsx
+++ src/components/styled/centered.tsx
@@ -1,12 +1,12 @@
 import { FunctionComponent } from "react";
 import styled from "styled-components";
 
-export const Centered = styled.div`
-  position: fixed;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-`;
+// export const Centered = styled.div`
+//   position: fixed;
+//   top: 50%;
+//   left: 50%;
+//   transform: translate(-50%, -50%);
+// `;
 
 const Container = styled.div`
   text-align: center;
@@ -19,13 +19,30 @@ const Recipient = styled.div`
   height: 100%;
 `;
 
-interface CenteredHorizontalProps {
+interface CenteredProps {
   className?: string;
 }
-export const CenteredHorizontal: FunctionComponent<CenteredHorizontalProps> = (
+export const CenteredHorizontal: FunctionComponent<CenteredProps> = (
   props
 ) => (
   <Container className={props.className}>
     <Recipient>{props.children}</Recipient>
   </Container>
 );
+
+const Recipient2 = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+
+export const Centered: FunctionComponent<CenteredProps> = (
+  props
+) => (
+  <Container className={props.className}>
+    <Recipient2>{props.children}</Recipient2>
+  </Container>
+);
+
